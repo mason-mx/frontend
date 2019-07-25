c1ded291825e5944192aeccd528b0bb375d38aa9
Mason <star.wang@live.com>
2019-07-25 20:02:45 +1200

    fixed error

---
+++ js/enterprise.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".fixed-top").removeClass("top-nav-collapse");
+    }
+  });
+});
